89ed48b8e4a4bb7f98aa226dc66cb5fb5edf13f6
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2024-10-16 15:41:13 -0300

    Primeiro Commit

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+/**
+ *
+ * @author Matheus
+ */
+public class ModuloConexao {
+    // método responsavel por estabelecer a conexão com o banco
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+    // a linha abaixo "chama" o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+    // Armazendo informações referente ao banco
+        String url= "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+    // Estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {  
+    //a linha abaixo serve de apoio para esclarecer o erro
+            return null;
+        }
+}
+}
